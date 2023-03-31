bfb154c2a23275e08bf4c1ee8fb2cf0506400a0a
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-03-31 10:55:10 -0600

    [BACKEND] Commented mongoose implementation

--- voyager/Backend/server.js
+++ voyager/Backend/server.js
@@ -19,13 +19,13 @@ app.use('/', (req, res) => {
 
 app.use('/test', testRouter);
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-});
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
