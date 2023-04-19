4d227eca0a7c946df6507319da3065028a29b42e
KaniMaki-bit <fer.ramrom02@gmail.com>
2023-04-19 10:38:21 -0600

    [DATABASE] General RDS connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const logger = require('./logger');
 const app = express();
-//For MongoDB connection
+//For RDS (AWS) database service connection
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri = process.env.URI;
+const { Client } = require('pg');
+const dbClient = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+})
+// try connection
+dbClient.connect((err) => {
+  if (err) {
+    console.error("Error de conexión:", err.stack);
+  } else {
+    console.log("Conexión exitosa a la base de datos de PostgreSQL a través de RDS");
+  }
+});
 
 app.use(logger);
 
@@ -21,27 +35,3 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor iniciado en el puerto 3000');
 });
-
-// MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-// Ping to check connection (optional)
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
