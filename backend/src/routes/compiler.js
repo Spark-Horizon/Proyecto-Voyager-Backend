0695f2f5bebb10d2764b966e38396393f257e054
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2023-05-15 02:42:20 -0600

    xd

--- backend/src/routes/compiler.js
+++ backend/src/routes/compiler.js
@@ -49,9 +49,9 @@ router.get('/problem/:id_problem', async (req, res) => {
 router.post('/problem/run', async (req, res) => {
     // console.log(req.body);
     if (!req.body || Object.keys(req.body).length === 0) { 
-        return res.status(400).send('Body data is undefined');
+        return res.status(300).send('Body data is undefined');
     }
-    
+
     const { code, driver, tests } = req.body;
 
 
