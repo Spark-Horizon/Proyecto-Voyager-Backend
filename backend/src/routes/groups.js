bb11d67102046a29a6ff86637659ad5889819059
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-22 10:41:42 -0600

    Route for post created and working

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+//Route for testing if route is working
 router.get('/', (req, res) => {
     res.send('groups route working');
+});
+
+//Route for post(button to create a group)
+router.post('/', async (req,res) => {
+    const {idMateriaGrupo, idDocente} = req.body;
+    res.send(`este es el idMateriaGrupo ${idMateriaGrupo} y este es el idDocente ${idDocente}`)
 });
 
 module.exports = router;
