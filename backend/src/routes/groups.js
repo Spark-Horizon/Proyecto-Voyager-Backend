e0be9c0fa7b76f159d0802557ac51b94df6f3545
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-23 12:34:01 -0600

    Stable Version with POSTMAN

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -22,14 +22,7 @@ router.post('/', async (req,res) => {
         console.log(idDocente ,typeof(idDocente));
         console.log(visibleGrupo ,typeof(visibleGrupo));
         client = await pool.connect();
-        // await client.query(`CALL agregargrupo('${visibleGrupo}', '${idMateriaGrupo}', '${idDocente}')`);
-        // Comenzar la transacción
-        // await client.query('BEGIN');    
-        // // Invocar el procedimiento almacenado
-        // await client.query(`SELECT agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, idDocente]);
-        // // Hacer commit a la transacción
-        // await client.query('COMMIT');
-        await client.query(`CALL "agregargrupo"(CAST($1 AS BOOLEAN), CAST($2 AS VARCHAR), CAST($3 AS VARCHAR))`, [visibleGrupo, idMateriaGrupo, idDocente]);
+        await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, idDocente]);
         res.status(201).json({ message: 'Group created succesfully' });
     }catch (err){
         await client.query('ROLLBACK');
