c5189668d9f1e0a6293ae76c17f350f4b7198fa4
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-23 15:19:41 -0600

    Filtering in post and get DONE

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -4,13 +4,13 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
-//Route for getting groups
-router.get('/', async (req, res) => {
-    //Code to get the groups
+//Route for getting groups from the actual user (Teacher)
+router.get('/:id_docente', async (req, res) => {
+    const { id_docente } = req.params;
     let client; //Declare the client variable
     try{
         client = await pool.connect();
-        const result = await client.query('SELECT * FROM grupos');
+        const result = await client.query('SELECT * FROM grupos WHERE id_docente = $1', [id_docente]);
         res.status(201).json(result.rows);
     }catch (err){
         console.error(err);
@@ -22,18 +22,15 @@ router.get('/', async (req, res) => {
 
 //Route for post(button to create a group)
 router.post('/', async (req,res) => {
-    const {idMateriaGrupo, idDocente, visibleGrupo} = await req.body; 
+    const {idMateriaGrupo, visibleGrupo, user} = await req.body; 
     //Checking if all data is present
-    if (!idMateriaGrupo || !idDocente || typeof visibleGrupo === 'undefined'){
+    if (!idMateriaGrupo || !user.id || typeof visibleGrupo === 'undefined'){
         return res.status(400).json({ error: 'Entry data not valid'});
     }
     let client; //Declare the client variable
     try{
-        console.log(idMateriaGrupo ,typeof(idMateriaGrupo));
-        console.log(idDocente ,typeof(idDocente));
-        console.log(visibleGrupo ,typeof(visibleGrupo));
         client = await pool.connect();
-        await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, idDocente]);
+        await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, user.id]);
         res.status(201).json({ message: 'Group created succesfully' });
     }catch (err){
         await client.query('ROLLBACK');
