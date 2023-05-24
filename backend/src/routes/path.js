367f0181e70f9108b9a99d1bd926d09b1fd71b6d
KaniMaki-bit <fer.ramrom02@gmail.com>
2023-05-24 02:15:22 -0600

    [DATABASE] Error handling path query

--- backend/src/routes/path.js
+++ backend/src/routes/path.js
@@ -13,13 +13,13 @@ router.get('/materia/:materia/', async (req, res) => {
     try {
         const client = await pool.connect()
         const result = await client.query(`
-        SELECT subtemas.id, subtemas.nombre, subtemas.id_tema, temas.nombre AS tema_nombre
-        FROM materias
-        INNER JOIN temas ON materias.id = temas.id_materia
-        INNER JOIN subtemas ON temas.id = subtemas.id_tema
-        WHERE materias.id = $1
-          AND subtemas.id >= (SELECT MIN(id_subtema) FROM estudiantes_subtemas);
-        `, [materia_ID])
+            SELECT subtemas.id, subtemas.nombre, subtemas.id_tema, temas.nombre AS tema_nombre
+            FROM materias
+            INNER JOIN temas ON materias.id = temas.id_materia
+            INNER JOIN subtemas ON temas.id = subtemas.id_tema
+            WHERE materias.id = $1
+            AND subtemas.id >= (SELECT MIN(id_subtema) FROM estudiantes_subtemas);
+            `, [materia_ID])
 
         if (result.rows[0] != null) {
             res.status(200).json(result.rows)
@@ -39,13 +39,13 @@ router.get('/unlocked/:matricula/:materia/', async (req, res) => {
     try {
         const client = await pool.connect()
         const result = await client.query(`
-        SELECT estudiantes_subtemas.id_subtema, estudiantes_subtemas.superado
-        FROM estudiantes_subtemas
-        INNER JOIN subtemas ON estudiantes_subtemas.id_subtema = subtemas.id
-        INNER JOIN temas ON subtemas.id_tema = temas.id
-        WHERE temas.id_materia = $1
-          AND id_estudiante = $2;
-        `, [materia_ID, estudiante_ID])
+            SELECT estudiantes_subtemas.id_subtema, estudiantes_subtemas.superado
+            FROM estudiantes_subtemas
+            INNER JOIN subtemas ON estudiantes_subtemas.id_subtema = subtemas.id
+            INNER JOIN temas ON subtemas.id_tema = temas.id
+            WHERE temas.id_materia = $1
+            AND id_estudiante = $2;
+            `, [materia_ID, estudiante_ID])
 
         if (result.rows[0] != null) {
             res.status(200).json(result.rows)
@@ -53,37 +53,64 @@ router.get('/unlocked/:matricula/:materia/', async (req, res) => {
             res.status(500).json({ "error": "Matriculo y/o materia no validos" })
         }
         client.release()
-    } catch(err){
+    } catch (err) {
         console.log(err)
         res.status(500).send(err)
     }
 })
 
-router.get('/progress/:matricula/:subtema/', async (req, res) => {
+router.get('/progress/:matricula/:subtema/:task_type', async (req, res) => {
     let estudiante_ID = req.params.matricula
     let subtema_ID = req.params.subtema
+    let task_type = req.params.task_type === 'MO' ? 'Opción múltiple' : 'Código';
+
     try {
         const client = await pool.connect()
         const result = await client.query(`
-        SELECT *
-        FROM estudiantes_ejercicios
-        WHERE id_estudiante = $1
-          AND id_ejercicio IN (
-            SELECT id
-            FROM ejercicios
-            WHERE id_subtema = $2
-          )
-          AND (correcto = false OR correcto IS NULL)
-        LIMIT 1;
-        `, [estudiante_ID, subtema_ID])
+            SELECT *
+            FROM practicas
+            WHERE id_estudiante = $1
+              AND id_ejercicio IN (
+                SELECT id
+                FROM ejercicios
+                WHERE id_subtema = $2
+                AND tipo = $3
+              )
+              AND (correcto = false OR correcto IS NULL)
+            LIMIT 1;
+            `, [estudiante_ID, subtema_ID, task_type])
 
         if (result.rows[0] != null) {
             res.status(200).json(result.rows)
         } else {
-            res.status(500).json({ "error": "Matriculo y/o subtema no validos" })
+            await client.query(`
+                CALL agregarPracticaRandRuta($1, $2, $3);
+                `, [estudiante_ID, subtema_ID, task_type])
+
+            const result = await client.query(`
+                SELECT *
+                FROM practicas
+                WHERE id_estudiante = $1
+                AND id_ejercicio IN (
+                    SELECT id
+                    FROM ejercicios
+                    WHERE id_subtema = $2
+                    AND tipo = $3
+                )
+                AND (correcto = false OR correcto IS NULL)
+                LIMIT 1;
+                `, [estudiante_ID, subtema_ID, task_type])
+            
+            console.log(result.rows[0])
+
+            if (result.rows[0] != null) {
+                res.status(200).json(result.rows)
+            } else {
+                res.status(500).json({ "error": "Matriculo y/o subtema no validos" })
+            }
         }
         client.release()
-    } catch(err){
+    } catch (err) {
         console.log(err)
         res.status(500).send(err)
     }
