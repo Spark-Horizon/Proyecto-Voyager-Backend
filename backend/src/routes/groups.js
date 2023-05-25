96b8164de123382218e8be77b2bd7f50161d93d3
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-24 21:27:18 -0600

    get improved version

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -4,68 +4,98 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
+const GET_STUDENT_QUERY = `SELECT * FROM grupos g
+                       INNER JOIN estudiantes_grupos eg ON g.id = eg.id_grupo
+                       WHERE eg.id_estudiante = $1`;
+const GET_TEACHER_QUERY = 'SELECT * FROM grupos WHERE id_docente = $1';
+
 //Route for getting groups from the actual user
 //The user could be a student or a teacher
-router.get('/:role/:id', async (req, res) => {
-  const { role } = req.params;
-  if (role === 'student') {
-    const { id:id_alumno } = req.params;
-    const query = `SELECT * FROM grupos g
-               INNER JOIN estudiantes_grupos eg ON g.id = eg.id_grupo
-               WHERE eg.id_estudiante = $1`;
-    const values = [id_alumno];
-    let client;
-    try {
-      client = await pool.connect();
-      const result = await client.query(query, values);//We need to query the query all the groups where the student is
-      res.status(200).json(result.rows);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error getting the data' });
-    } finally {
-      client.release();
-    }
-  } else if (role === 'teacher') {
-    const { id: id_docente } = req.params;
-    let client; //Declare the client variable
-    try {
-      client = await pool.connect();
-      const result = await client.query('SELECT * FROM grupos WHERE id_docente = $1', [id_docente]);
-      res.status(200).json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Error getting the data' });
-    } finally {
-      client.release();
-    }
-  } else {
-    console.error('Not a valid role');
-    res.status(404).json({ error: 'Not a valid role' });
-  }
-});
+router.get('/:role/:id', async (req, res, next) => {
+  const { role, id } = req.params;
+  let query;
 
-//Route for post(button to create a group)
-router.post('/', async (req, res) => {
-  const { idMateriaGrupo, visibleGrupo, user } = req.body;
-  //Checking if all data is present
-  if (!idMateriaGrupo || !user.id || typeof visibleGrupo === 'undefined') {
-    return res.status(400).json({ error: 'Entry data not valid' });
+  switch (role) {
+    case 'student':
+      query = GET_STUDENT_QUERY;
+      break;
+    case 'teacher':
+      query = GET_TEACHER_QUERY;
+      break;
+    default:
+      return res.status(404).json({ error: 'Not a valid role' });
   }
-  let client; //Declare the client variable
+
   try {
-    client = await pool.connect();
-    await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, user.id]);
-    res.status(201).json({ message: 'Group created succesfully' });
-  } catch (err) {
-    await client.query('ROLLBACK');
-    console.error('Error adding the group', err);
-    res.status(500).json({ error: 'Server Internal Error' });
-  } finally {
-    client.release();
+    const client = await pool.connect();
+    const result = await client.query(query, [id]);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error(error);
+    //Errors managed by middleware
+    next(error);
   }
 });
 
-// Route for delete
+//Route for post(button to create a group) if teacher
+//Route for post(button to enter to a group) if student
+//To enter to a group we need to call the procedure agregarEstudianteGrupo(IN codigo_grupo TEXT, IN id_estudiante_nuevo VARCHAR(10))
+//Teacher JSON for post:
+// {
+//   "idMateriaGrupo": "TC1028",
+//   "visibleGrupo": true,
+//   "user" : {
+//   "id": "L01732005",
+//   "name": "Francisco",
+//   "lastname1": "Rocha",
+//   "lastname2": "Juárez",
+//   "role": "teacher"
+//   }
+// }
+//Student JSON for post:
+//{
+//  "codigoGrupo": "DEE8C67B",
+//   "user" : {
+//   "id": "A01732007",
+//   "name": "Francisco",
+//   "lastname1": "Rocha",
+//   "lastname2": "Juárez",
+//   "role": "student"
+//   }
+//}
+
+// router.post('/', async (req, res) => {
+//   switch (role){
+//     case 'student':
+//       //Instructions
+//       break;
+//     case 'teacher':
+//       //Instructions
+//       break;
+//     default:
+//       return res.status(404).json({ error: 'Not a valid role' })
+//   }
+//   const { idMateriaGrupo, visibleGrupo, user } = req.body;
+//   //Checking if all data is present
+//   if (!idMateriaGrupo || !user.id || typeof visibleGrupo === 'undefined') {
+//     return res.status(400).json({ error: 'Entry data not valid' });
+//   }
+//   let client; //Declare the client variable
+//   try {
+//     client = await pool.connect();
+//     await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, user.id]);
+//     res.status(201).json({ message: 'Group created succesfully' });
+//   } catch (err) {
+//     await client.query('ROLLBACK');
+//     console.error('Error adding the group', err);
+//     res.status(500).json({ error: 'Server Internal Error' });
+//   } finally {
+//     client.release();
+//   }
+// });
+
+// Route for delete (button to delete a group) if teacher
+//Route for delete (button to get out of the group) if student
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
 
