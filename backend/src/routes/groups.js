b924d6c9d81641ee7bec02209e39d6ab61c07e75
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-24 21:42:48 -0600

    POST already working

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -4,10 +4,18 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
+//GET QUERYS
 const GET_STUDENT_QUERY = `SELECT * FROM grupos g
                        INNER JOIN estudiantes_grupos eg ON g.id = eg.id_grupo
                        WHERE eg.id_estudiante = $1`;
 const GET_TEACHER_QUERY = 'SELECT * FROM grupos WHERE id_docente = $1';
+
+//POST QUERYS
+const ADD_GROUP_PROCEDURE = `CALL agregarGrupo($1, $2, $3)`;
+const ADD_STUDENT_TO_GROUP_PROCEDURE = `CALL agregarEstudianteGrupo($1, $2)`;
+
+//DELETE QUERYS
+
 
 //Route for getting groups from the actual user
 //The user could be a student or a teacher
@@ -53,7 +61,7 @@ router.get('/:role/:id', async (req, res, next) => {
 //   }
 // }
 //Student JSON for post:
-//{
+// {
 //  "codigoGrupo": "DEE8C67B",
 //   "user" : {
 //   "id": "A01732007",
@@ -62,37 +70,61 @@ router.get('/:role/:id', async (req, res, next) => {
 //   "lastname2": "Juárez",
 //   "role": "student"
 //   }
-//}
+// }
 
-// router.post('/', async (req, res) => {
-//   switch (role){
-//     case 'student':
-//       //Instructions
-//       break;
-//     case 'teacher':
-//       //Instructions
-//       break;
-//     default:
-//       return res.status(404).json({ error: 'Not a valid role' })
-//   }
-//   const { idMateriaGrupo, visibleGrupo, user } = req.body;
-//   //Checking if all data is present
-//   if (!idMateriaGrupo || !user.id || typeof visibleGrupo === 'undefined') {
-//     return res.status(400).json({ error: 'Entry data not valid' });
-//   }
-//   let client; //Declare the client variable
-//   try {
-//     client = await pool.connect();
-//     await client.query(`CALL agregarGrupo($1, $2, $3)`, [visibleGrupo, idMateriaGrupo, user.id]);
-//     res.status(201).json({ message: 'Group created succesfully' });
-//   } catch (err) {
-//     await client.query('ROLLBACK');
-//     console.error('Error adding the group', err);
-//     res.status(500).json({ error: 'Server Internal Error' });
-//   } finally {
-//     client.release();
-//   }
-// });
+router.post('/', async (req, res) => {
+  const { idMateriaGrupo, visibleGrupo, codigoGrupo, user } = req.body;
+  let client;
+
+  if (!user || !user.role) {
+    return res.status(400).json({ error: 'Role missing in user data' });
+  }
+
+  const { role, id } = user;
+
+  switch (role){
+    case 'student':
+      // Verify if data is present
+      if (!codigoGrupo || !id) {
+        return res.status(400).json({ error: 'Entry data not valid for student' });
+      }
+      
+      try {
+        client = await pool.connect();
+        await client.query(ADD_STUDENT_TO_GROUP_PROCEDURE, [codigoGrupo, id]);
+        res.status(201).json({ message: 'Student added to group successfully' });
+      } catch (err) {
+        await client.query('ROLLBACK');
+        console.error('Error adding the student to the group', err);
+        res.status(500).json({ error: 'Server Internal Error' });
+      } finally {
+        client.release();
+      }
+      break;
+
+    case 'teacher':
+      // Verify if data is present
+      if (!idMateriaGrupo || !id || typeof visibleGrupo === 'undefined') {
+        return res.status(400).json({ error: 'Entry data not valid for teacher' });
+      }
+
+      try {
+        client = await pool.connect();
+        await client.query(ADD_GROUP_PROCEDURE, [visibleGrupo, idMateriaGrupo, id]);
+        res.status(201).json({ message: 'Group created successfully' });
+      } catch (err) {
+        await client.query('ROLLBACK');
+        console.error('Error adding the group', err);
+        res.status(500).json({ error: 'Server Internal Error' });
+      } finally {
+        client.release();
+      }
+      break;
+
+    default:
+      return res.status(404).json({ error: 'Not a valid role' });
+  }
+});
 
 // Route for delete (button to delete a group) if teacher
 //Route for delete (button to get out of the group) if student
