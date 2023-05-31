22f91e9cdbc957b1b0940e2e31b8502f9d54b1f4
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-05-31 12:30:57 -0600

    GET_ATTEMPS_QUERY DONE

--- backend/src/routes/quizAttemp.js
+++ backend/src/routes/quizAttemp.js
@@ -5,14 +5,27 @@ const router = express.Router();
 const pool = require('../../db/index');
 
 //GET QUERYS
+const GET_ATTEMPS_QUERY = 'SELECT * FROM intentos WHERE id_estudiante = $1 AND id_actividad = $2';
 
 //POST QUERYS
 
 //DELETE QUERYS
 
 //Function to get all the attemps from an specific activity and a specific user(Student).
-router.get('/', (req,res) => {
-    res.send('quizAttemp route working');
+router.get('/:id_student/:id_activity', async (req,res,next) => {
+    const { id_student, id_activity } = req.params;
+    const VALUES = [id_student, id_activity];
+    let client;
+    try{
+        client = await pool.connect();
+        const result = await client.query(GET_ATTEMPS_QUERY, VALUES);
+        res.status(200).json(result.rows);
+    }catch (error){
+        console.error(error);
+        next(error); //Errors managed by middleware
+    }finally{
+        client.release();
+    }
 });
 
 
