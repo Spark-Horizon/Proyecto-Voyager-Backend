7cec957c040a93275b7b5d9bb0b7a4cea389be46
Francisco Rocha <73204906+Rocha134@users.noreply.github.com>
2023-06-07 15:43:48 -0600

    Get exercises id and type from an activity

--- backend/src/routes/quizStudent.js
+++ backend/src/routes/quizStudent.js
@@ -4,8 +4,30 @@ const router = express.Router();
 
 const pool = require('../../db/index');
 
-router.get('/', (req, res) => {
-    res.send('quizStudent route working');
+//GET QUERYS
+const GET_EXERCISES_QUERY = `SELECT ejercicios.id, ejercicios.tipo
+FROM ejercicios
+INNER JOIN actividades_ejercicios
+ON ejercicios.id = actividades_ejercicios.id_ejercicio
+WHERE actividades_ejercicios.id_actividad = $1`;
+
+//POST QUERYS
+
+//DELETE QUERYS
+
+//Get exercises id and type from an activity
+router.get('/:id_activity', async (req, res) => {
+    const params = [req.params.id_activity];
+    let client;
+    try{
+        client = await pool.connect();
+        const result = await client.query(GET_EXERCISES_QUERY,params);
+        res.status(200).json(result.rows);
+    }catch (error){
+        console.error(error);
+    }finally{
+        client.release();
+    }
 });
 
 module.exports = router;
