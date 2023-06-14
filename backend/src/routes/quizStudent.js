91c96143a4e27b739321371e43710bd6597c1622
KaniMaki-bit <fer.ramrom02@gmail.com>
2023-06-13 19:59:58 -0600

    [FUNC] asignacion intento

--- backend/src/routes/quizStudent.js
+++ backend/src/routes/quizStudent.js
@@ -30,4 +30,48 @@ router.get('/:id_activity', async (req, res) => {
     }
 });
 
+// busca si ya tiene un intento del quizz sin completar, o crea un intento nuevo para el quizz (depende del user_id)
+router.get('/getorset/:matricula/:quiz', async (req, res) => {
+    const estudiante_ID = req.params.matricula;
+    const quizz_ID = req.params.quiz
+    const intentoQuery = `
+      SELECT *
+      FROM intentos
+      WHERE id_estudiante = $1
+      AND id_actividad = $2
+      AND fin IS NULL
+    `;
+
+    let client;
+
+    try {
+        client = await pool.connect();
+
+        let result = await client.query(intentoQuery, [estudiante_ID, quizz_ID])
+    
+        if (result.rows[0] == null) {
+            await client.query(`CALL agregarIntento($1, $2);`, [
+                estudiante_ID,
+                quizz_ID
+            ]);
+
+            result = await client.query(intentoQuery, [estudiante_ID, quizz_ID]);
+            console.log(result.rows);
+        }
+        
+
+        if (result.rows[0] != null) {
+            res.status(200).json(result.rows);
+        } else {
+            res.status(500).json({ error: 'Matriculo y/o quiz no validos' });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    } finally {
+        client.release();
+    }
+});
+
 module.exports = router;
